d3dde29c83562e314648a2961c7c912a0a08ca1f
Mmati1996 <mat.duk.at@gmail.com>
2020-11-05 00:21:00 +0100

    some refactoring

--- back/src/main/java/com/example/back/controllers/DataController.java
+++ back/src/main/java/com/example/back/controllers/DataController.java
@@ -2,7 +2,6 @@ package com.example.back.controllers;
 
 import com.example.back.models.User;
 import com.example.back.repositories.UserRepository;
-import com.example.back.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,25 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DataController {
 
-    private static UserService userService;
-
-    public DataController(UserService userService) {
-        this.userService = userService;
-    }
-
     @Autowired
-    UserRepository repository;
-
+    UserRepository userRepository;
 
     //http://localhost:8080/user?id=1
     @GetMapping("/user")
-    public static User getUser(@RequestParam int id){
-        return userService.getUserById(id);
+    public User getUser(@RequestParam int id){
+        return userRepository.findById(id).get();
     }
 
+    //http://localhost:8080/users/all
     @GetMapping("/users/all")
     public Iterable<User> getAllUsers(){
-        return repository.findAll();
+        return userRepository.findAll();
     }
 
 }
