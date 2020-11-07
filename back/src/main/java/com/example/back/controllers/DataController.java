dfa9b16a3c4c85bd984de62c15cb3d85df868a4d
Mmati1996 <mat.duk.at@gmail.com>
2020-11-07 15:31:00 +0100

    simple method to receive data from frontend added

--- back/src/main/java/com/example/back/controllers/DataController.java
+++ back/src/main/java/com/example/back/controllers/DataController.java
@@ -2,17 +2,19 @@ package com.example.back.controllers;
 
 import com.example.back.models.User;
 import com.example.back.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class DataController {
 
     @Autowired
     UserRepository userRepository;
-
     //http://localhost:8080/user?id=1
     @GetMapping("/user")
     public User getUser(@RequestParam int id){
@@ -24,5 +26,10 @@ public class DataController {
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
     }
+
+    @PutMapping("/adduser")
+    public void addUser(){
+        log.info("user added");
+    };
 
 }
