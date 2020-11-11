f4fccc4cd543816c4c3e12047808ecdd48b19330
Mmati1996 <mat.duk.at@gmail.com>
2020-11-11 20:18:16 +0100

    cross origin annotation added

--- back/src/main/java/com/example/back/controllers/DataController.java
+++ back/src/main/java/com/example/back/controllers/DataController.java
@@ -5,12 +5,10 @@ import com.example.back.repositories.UserRepository;
 import com.example.back.services.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class DataController {
 
     //TODO -- change repository to service
@@ -37,8 +35,8 @@ public class DataController {
             return userRepository.findAll();
         }
 
-    @PostMapping("/register")
-    public ResponseEntity addUser(@RequestParam String username,@RequestParam String password){
+        @PostMapping("/register")
+        public ResponseEntity addUser(@RequestParam String username,@RequestParam String password){
         User user = new User(username,password);
         userRepository.save(user);
         //service.addUser(user);
