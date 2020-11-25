2a1919cf7b9e18cc6cdcbd52c605637b5df30e43
Mmati1996 <mat.duk.at@gmail.com>
2020-11-25 18:41:48 +0100

    teble operating methods

--- back/src/main/java/com/example/back/controllers/DataController.java
+++ back/src/main/java/com/example/back/controllers/DataController.java
@@ -21,7 +21,7 @@ public class DataController {
     //TODO -- change repository to service
     UserRepository userRepository;
     TableRepository tableRepository;
-    IdsRepository idsRepository;
+    IdsRepository idRepository;
 
 
     private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
@@ -29,11 +29,11 @@ public class DataController {
 
     private UserServiceImpl userService;
 
-    public DataController(UserServiceImpl service, UserRepository userRepository, TableRepository tableRepository,IdsRepository idsRepository) {
+    public DataController(UserServiceImpl service, UserRepository userRepository, TableRepository tableRepository,IdsRepository idRepository) {
         this.userService = service;
         this.userRepository = userRepository;
         this.tableRepository = tableRepository;
-        this.idsRepository =  idsRepository;
+        this.idRepository = idRepository;
     }
 
 
@@ -106,18 +106,6 @@ public class DataController {
         return tableRepository.findAll();
     }
 
-    @PostMapping("tables/add")
-    public Response addTable(@RequestParam String token, @RequestParam String tableName){
-        Table table = new Table(tableName);
-        User user = findUserByToken(token);
-        if (user != null){
-            tableRepository.save(table);
-            idsRepository.save(new Id(user.getId(),table.getId()));
-            return new Response(true,"user added","");
-        }
-        return new Response(false,"","wrong token");
-    }
-
 
     public User findUserByToken(String token){
         ArrayList<User> users = (ArrayList<User>) userRepository.findAll();
@@ -129,5 +117,118 @@ public class DataController {
             }
         }
         return null;
+    }
+
+    public Table findTableByName(String name){
+        ArrayList<Table> tables = (ArrayList<Table>) tableRepository.findAll();
+        for (Table tab : tables){
+            if (tab.getName().equals(name)){
+                return tab;
+            }
+
+        }
+        return null;
+    }
+
+    public User findUserByLogin(String login){
+        for (User user : userRepository.findAll()){
+            if (user.getLogin().equals(login)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public boolean canTableBeAccessed(String token, String tableName){
+        Table table = findTableByName(tableName);
+        if ( table==null ){
+            return false;
+        }
+        ArrayList<Id> ids = (ArrayList<Id>) idRepository.findAll();
+        for (Id id : ids){
+            if(  ( id.getUser_id() == findUserByToken(token).getId() ) && (id.getTable_id()) == table.getId()  ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @PutMapping("/userByToken")
+    public User findUserByTokenRequest(@RequestBody String token){
+        ArrayList<User> users = (ArrayList<User>) userRepository.findAll();
+        for ( User us : users  ){
+            if (us.getToken()!=null){
+                if (us.getToken().equals(token)){
+                    return us;
+                }
+            }
+        }
+        return null;
+    }
+
+    @PostMapping("tables/add")//tableName
+    public Response addTable(@RequestHeader String token, @RequestParam String param1){
+
+        Table table = new Table(param1);
+        User user = findUserByToken(token);
+        if (user != null){
+            tableRepository.save(table);
+            idRepository.save(new Id(user.getId(),table.getId()));
+            return new Response(true,"added","");
+        }
+        return new Response(false,"","wrong token");
+    }
+
+    @PostMapping("tables/delete")//tableName
+    public Response deleteTable (@RequestHeader String token, @RequestParam String param1){
+        Table table = findTableByName(param1);
+        if (canTableBeAccessed(token, param1)){
+            ArrayList<Id> ids = (ArrayList<Id>) idRepository.findAll();
+            for (Id id : ids){
+                if (id.getTable_id()==table.getId()){
+                    idRepository.delete(id);
+                }
+
+            }
+            tableRepository.delete(table);
+            return new Response(true,"deleted","");
+        }
+        return new Response(false,"","this user cannot access given table");
+    }
+
+    @PostMapping("tables/changeName")//tableToChange newName
+    public Response ChangeTableName(@RequestHeader String token, @RequestBody Message message){
+        if( !canTableBeAccessed(token,message.getParam1() ) ){
+            return new Response(false,"","this user cannot access given table");
+        }
+        Table tab = findTableByName(message.getParam1());
+        tab.setName(message.getParam2());
+        tableRepository.save(tab);
+        return  new Response(true,"changed name to "+message.getParam2(),"");
+    }
+
+    @PostMapping("tables/addUser")//table userToAdd(login)
+    public Response addUserToTable(@RequestHeader String token,@RequestBody Message message){
+        if (canTableBeAccessed(token,message.getParam1())){
+             idRepository.save( new Id (findUserByLogin(message.getParam2()).getId(),findTableByName(message.getParam1()).getId())  );
+            return new Response (false,"user added","");
+        }
+        return new Response (false,"","this user cannot access given table");
+    }
+
+    @PostMapping("tables/deleteUser")//table user(login)
+    public Response deleteUser(@RequestHeader String token, @RequestBody Message message){
+        if (!canTableBeAccessed(token,message.getParam1())){
+            return new Response(false,"","this user cannot access given table");
+        }
+        int id = findUserByLogin(message.getParam2()).getId();
+        for (Id ids : idRepository.findAll()){
+            if ( (ids.getUser_id() == id) &&  ( ids.getTable_id() == findTableByName(message.getParam1()).getId() ) ){
+                idRepository.delete(ids);
+                return new Response(true,"user " + message.getParam2() +" deleted from "+message.getParam1(),"");
+            }
+        }
+        return new Response(false,message.getParam2()+" does not have acces to "+message.getParam1()+" so it cant be deleted","");
     }
 }
---
+++ back/src/main/java/com/example/back/responseModels/Message.java
@@ -0,0 +1,15 @@
+package com.example.back.responseModels;
+
+
+import lombok.Data;
+
+@Data
+public class Message {
+    String param1;
+    String param2;
+
+    public Message(String par1, String par2) {
+        this.param1 = par1;
+        this.param2 = par2;
+    }
+}
