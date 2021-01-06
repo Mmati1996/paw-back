eb3e204fdf9438c9acbad89bb9ec5d1b32549f44
Mmati1996 <mat.duk.at@gmail.com>
2021-01-06 22:56:31 +0100

    test3

--- back/src/main/java/com/example/back/responseModels/CardModifier.java
+++ back/src/main/java/com/example/back/responseModels/CardModifier.java
@@ -1,11 +1,10 @@
 package com.example.back.responseModels;
 
 
-import com.example.back.models.Task2;
 import lombok.Data;
 
 @Data
 public class CardModifier {
-    Task2[] param1;
+    //Task2[] param1;
     int param2;
 }
